647806c7de3f333848d3341d3e0ef3dfaba0bb88
Thomas AWOUNFOUET <thomas.awounfouet@gmail.com>
2023-10-07 01:48:09 +0200

    First pass at working Add Cheese form

--- everycheese/cheeses/views.py
+++ everycheese/cheeses/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
 
+
 # Create your views here.
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Cheese
+
+
 
 class CheeseListView(ListView):
     model = Cheese
 
+
 class CheeseDetailView(DetailView):
     model = Cheese
+
+
+class CheeseCreateView(CreateView):
+    model = Cheese
+
+    fields = [
+        "name",
+        "description",
+        "firmness",
+        "country_of_origin",
+    ]
