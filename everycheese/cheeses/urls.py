e10046f90b762069e1a3172324df1f685809683f
Thomas AWOUNFOUET <thomas.awounfouet@gmail.com>
2023-10-07 00:52:06 +0200

    Implement cheese detail page

--- everycheese/cheeses/urls.py
+++ everycheese/cheeses/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = "cheeses"
 urlpatterns = [
     path(route="", view=views.CheeseListView.as_view(), name="list"),
+    path(route="<slug:slug>/", view=views.CheeseDetailView.as_view(), name="detail"),
 ]
