661c0c5bba5200b65e5d4d73ec75979b1f696382
Thomas AWOUNFOUET <thomas.awounfouet@gmail.com>
2023-10-07 01:28:59 +0200

    Implement cheese country of origin

--- everycheese/cheeses/models.py
+++ everycheese/cheeses/models.py
@@ -2,12 +2,14 @@ from django.db import models
 from autoslug import AutoSlugField
 from model_utils.models import TimeStampedModel
 
+from django_countries.fields import CountryField
+
 
 class Cheese(TimeStampedModel):
     name = models.CharField("Name of Cheese", max_length=255)
-    slug = AutoSlugField("Cheese Address", unique=True,
-                                        always_update=False,
-                                        populate_from="name")
+    slug = AutoSlugField(
+        "Cheese Address", unique=True, always_update=False, populate_from="name"
+    )
     description = models.TextField("Description", blank=True)
 
     class Firmness(models.TextChoices):
@@ -18,9 +20,14 @@ class Cheese(TimeStampedModel):
         HARD = "hard", "Hard"
 
     # Other Fields Here...
-    firmness = models.CharField("Firmness", max_length=20,
-                                            choices=Firmness.choices,
-                                            default=Firmness.UNSPECIFIED)
+    firmness = models.CharField(
+        "Firmness",
+        max_length=20,
+        choices=Firmness.choices,
+        default=Firmness.UNSPECIFIED,
+    )
+
+    country_of_origin = CountryField("Country of Origin", blank=True)
 
     def __str__(self):
         return self.name
